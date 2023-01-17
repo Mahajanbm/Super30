ec98b834f2e0e202d1020516425e978a2e14fde8
Mahajanbm <108041272+Mahajanbm@users.noreply.github.com>
2023-01-17 11:50:09 +0530

    Add files via upload

---
+++ CoreJavaSy.java
@@ -0,0 +1,9 @@
+class CoreJavaSy
+
+{
+public static void main(String args[])
+{
+System.out.println("Welcome to MIT");
+
+}
+}
---
+++ Sy.java
@@ -0,0 +1,20 @@
+class Sy
+{
+public static void main(String args[])
+{
+ int a=50;
+ int b=60;
+int Result=a+b;
+
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+System.out.println("Addition of 2 numbers is : "+Result);
+
+System.out.println("Substraction of 2 numbers is : "+sub);
+System.out.println("Multiplication of 2 numbers is : "+mul);
+System.out.println("Division of 2 numbers is : "+div);
+}
+
+
+}
